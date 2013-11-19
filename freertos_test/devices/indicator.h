a536d00e6122f665ed22b0e1bf5456bd9a051e80
nowhard <nowhard@mail.ru>
2013-11-19 16:19:04 +0300

    Нстроено на тестовый проект-стенд

--- freertos_test/devices/indicator.h
+++ freertos_test/devices/indicator.h
@@ -39,8 +39,8 @@ struct indicator{
 
 #define IND_ALL_NUM			20//общее количество индикаторов
 
-#define IND_SPI_BUS_1_NUM	10 //количество индикаторов на шине
-#define IND_SPI_BUS_2_NUM	10
+#define IND_SPI_BUS_1_NUM	12 //количество индикаторов на шине
+#define IND_SPI_BUS_2_NUM	6
 #define IND_SPI_BUS_3_NUM	0
 
 uint8_t indicators_init(void);//инициализация и выделение памяти под индикаторы
